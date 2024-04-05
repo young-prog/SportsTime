9ebbf1e4bfa8980038d956c8f16b2de2bdad6df5
ilya <166118020+young-prog@users.noreply.github.com>
2024-04-05 20:34:58 +0700

    Add files via upload

---
+++ src/js/main.js
@@ -0,0 +1,25 @@
+
+document.querySelector('.n1').addEventListener('click', function () {
+    document.querySelector('.block1').scrollIntoView({ behavior: 'smooth' });
+});
+document.querySelector('.n2').addEventListener('click', function () {
+    document.querySelector('.block2').scrollIntoView({ behavior: 'smooth' });
+});
+document.querySelector('.n3').addEventListener('click', function () {
+    document.querySelector('.block3').scrollIntoView({ behavior: 'smooth' });
+});
+document.querySelector('.button1').addEventListener('click', function () {
+    document.querySelector('.ft').scrollIntoView({ behavior: 'smooth' });
+});
+document.querySelector('.button2').addEventListener('click', function () {
+    document.querySelector('.st').scrollIntoView({ behavior: 'smooth' });
+});
+document.querySelector('.button3').addEventListener('click', function () {
+    document.querySelector('.dt').scrollIntoView({ behavior: 'smooth' });
+});
+document.querySelector('.button5').addEventListener('click', function () {
+    document.querySelector('.tnr').scrollIntoView({ behavior: 'smooth' });
+});
+document.querySelector('.button4').addEventListener('click', function () {
+    document.querySelector('.smesht').scrollIntoView({ behavior: 'smooth' });
+});
